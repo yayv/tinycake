709f61d0267337b222164094323060be704bfe2c
Liu Ce <yayv.cn@gmail.com>
2016-07-24 16:55:47 +0800

    增加数据库连接的测试配置

--- install/configs/cfg.default.php
+++ install/configs/cfg.default.php
@@ -5,3 +5,10 @@ $CONFIG['compiled_template'] = 'v/_run';
 $CONFIG['baseurl'] = 'http://install.localhost';
 $CONFIG['basedir'] = '/Data/tinycake/install';
 
+$CONFIG['database'] = array(
+	'host' 		=> '127.0.0.1',
+	'port' 		=> '3306',
+	'username' 	=> 'office',
+	'password' 	=> 'officedb',
+	'database' 	=> 'topbox',
+	);
