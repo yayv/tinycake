b3d7d2e638a4ce5f4ac89e4dfaae0b7e2f86adda
闫鑫瑶 <37734020@qq.com>
2020-02-18 14:53:58 +0800

    添加方法

--- cake/libraries/cls.mysql.php
+++ cake/libraries/cls.mysql.php
@@ -1,373 +1,424 @@
 <?php
 /**
-  * Note: 这个类是为了对之前所用的mysql类进行过度而进行封装的。将来，有可能在tinycake类中直接使用mysqli
-  * 
-  */
+ * Note: 这个类是为了对之前所用的mysql类进行过度而进行封装的。将来，有可能在tinycake类中直接使用mysqli
+ *
+ */
 class mysql
 {
-	// mysqli 对象
-	var $mi;
-	// 日志文件句柄
-	var $logfile;
-	// 链接mysql服务器的参数
-	var $db;
+    // mysqli 对象
+    public $mi;
+    // 日志文件句柄
+    public $logfile;
+    // 链接mysql服务器的参数
+    public $db;
 
-    var $connected;
+    public $connected;
 
-    function __construct($server)
+    public function __construct($server)
     {
-        $this->connected = false; 
+        $this->connected = false;
 
-		// 如果$server有配置，则用server,如果没有，则使用defcfg的默认配置
-		$defcfg = array(
-				"host"=>'127.0.0.1',
-				"port"=>"3306",
-				'username'=>'', 
-				'password'=>'', 
-				'database'=>'', 
-				'socket'=>'', 
-				'charset'=>'utf8',
-			);
+        // 如果$server有配置，则用server,如果没有，则使用defcfg的默认配置
+        $defcfg = array(
+            "host"     => '127.0.0.1',
+            "port"     => "3306",
+            'username' => '',
+            'password' => '',
+            'database' => '',
+            'socket'   => '',
+            'charset'  => 'utf8',
+        );
 
-        if(!is_array($server)) $server = array();
+        if (!is_array($server)) {
+            $server = array();
+        }
 
-		$res = array_diff_key($defcfg, $server);
+        $res = array_diff_key($defcfg, $server);
 
         $this->logfile = "php://output";
 
-		$this->db = array_merge($res, $server);
+        $this->db = array_merge($res, $server);
 
-		// 建立链接
+        // 建立链接
         try
         {
             $this->mi = new mysqli(
-                    $this->db['host'], $this->db['username'], $this->db['password'], 
-                    $this->db['database'], $this->db['port'],$this->db['socket']
-                );
-        }
-        catch(Exception $e)
-        {
-            return false;
+                $this->db['host'], $this->db['username'], $this->db['password'],
+                $this->db['database'], $this->db['port'], $this->db['socket']
+            );
+        } catch (Exception $e) {
+            die('hhh');
         }
 
-        if(!isset($this->errno) || $this->errno===NULL)
-        {
+        if (!isset($this->errno) || $this->errno === null) {
             $this->connected = true;
-            $this->mi->set_charset($this->db['charset']);                
-        }
-    }
-
-    function isConnected()
-    {
-        return $this->connected ;
-    }
-
-    function setLogfile($logfile='php://output')
-    {
-		$this->logfile = $logfile;
-    }
-
-    function connect()
-    {
-		$ret = $this->mi->real_connect(
-					$this->db['host'], 
-					$this->db['username'], 
-					$this->db['password'], 
-					$this->db['database'], 
-					$this->db['port'], 
-					$this->db['socket']
-			);
-
-        if($ret)
-        {
             $this->mi->set_charset($this->db['charset']);
         }
-        else
-        {
-        	$this->show_error($this->mi->connect_error);
-        }		
     }
 
-    function select_db($database)
+    public function isConnected()
     {
-    	// inherent
-    	$this->mi->select_db($database);
+        return $this->connected;
     }
 
-
-    function get_error()
+    public function setLogfile($logfile = 'php://output')
     {
-        if($this->mi->connect_errno)
-        {
-            $error['sql']           = $this->mi->escape_string;
-            $error['number']        = $this->mi->connect_errno;
-            $error['description']    = $this->mi->connect_error;
+        $this->logfile = $logfile;
+    }
+
+    public function connect()
+    {
+        $ret = $this->mi->real_connect(
+            $this->db['host'],
+            $this->db['username'],
+            $this->db['password'],
+            $this->db['database'],
+            $this->db['port'],
+            $this->db['socket']
+        );
+
+        if ($ret) {
+            $this->mi->set_charset($this->db['charset']);
+        } else {
+            $this->show_error($this->mi->connect_error);
         }
-        else 
-        {
-            $error['sql']           = $this->mi->escape_string;
-            $error['number']        = $this->mi->errno;
-            $error['description']    = $this->mi->error;
+    }
+
+    public function select_db($database)
+    {
+        // inherent
+        $this->mi->select_db($database);
+    }
+
+    public function get_error()
+    {
+        if ($this->mi->connect_errno) {
+            $error['sql']         = $this->mi->escape_string;
+            $error['number']      = $this->mi->connect_errno;
+            $error['description'] = $this->mi->connect_error;
+        } else {
+            $error['sql']         = $this->mi->escape_string;
+            $error['number']      = $this->mi->errno;
+            $error['description'] = $this->mi->error;
         }
         return $error;
     }
 
-    function show_error($title)
+    public function show_error($title)
     {
         $content = "--------------------\n";
-        $content.= $title."\n";
-        $content.= $this->mi->connect_errno?$this->mi->connect_error:$this->mi->error;
-        $content.= "\n";
-        $content.= "$this->last_sql\n";
+        $content .= $title . "\n";
+        $content .= $this->mi->connect_errno ? $this->mi->connect_error : $this->mi->error;
+        $content .= "\n";
+        $content .= "$this->last_sql\n";
 
         error_log($content, 3, $this->logfile);
 
         return $content;
     }
 
-    function affected_rows()
+    public function affected_rows()
     {
-    	// inherent
-		return $this->mi->affected_rows;
+        // inherent
+        return $this->mi->affected_rows;
     }
 
-    function query($sql)
+    public function query($sql)
     {
         // result maybe false
-		$this->lastresult = $this->mi->query($sql);
-		return $this->lastresult;
+        $this->lastresult = $this->mi->query($sql);
+        return $this->lastresult;
     }
 
-    function fetch_row($result)
+    public function fetch_row($result)
     {
-        if($result)
-        {
+        if ($result) {
             $row = $result->fetch_row();
             return $row;
-        }
-        else
+        } else {
             return false;
+        }
+
     }
 
-    function insert_id()
+    public function insert_id()
     {
-    	// inherent
-    	return $this->mi->insert_id;
+        // inherent
+        return $this->mi->insert_id;
     }
 
-    function num_rows($result)
+    public function num_rows($result)
     {
 
-        return $result->num_rows;
+        return $result->num_rows();
     }
 
-    function fetch_object($result)
+    public function fetch_object($result)
     {
         $ret = $result->fetch_object();
 
         return $ret;
     }
 
-    function fetch_array($result)
+    public function fetch_array($result)
     {
         $ret = $result->fetch_array();
         return $ret;
     }
 
-    function fetch_assoc($result)
+    public function fetch_assoc($result)
     {
         $ret = $result->fetch_assoc();
         return $ret;
     }
 
-    function fetch_one_object($sql)
+    public function fetch_one_object($sql)
     {
         $query = $this->query($sql);
 
-        if($query)
+        if ($query) {
             return $this->fetch_object($query);
-        else
+        } else {
             return false;
-    }
-
-    function fetch_one_array($sql)
-    {
-        $list         = $this->query($sql);
-
-        if($list)
-        {
-            $list_array = $this->fetch_array($list);
-            return $list_array;            
         }
-        else
-            return false;
+
     }
 
-    function fetch_one_assoc($sql)
+    public function fetch_one_array($sql)
     {
-        $list         = $this->query($sql);
-        if($list)
-        {
+        $list = $this->query($sql);
+
+        if ($list) {
+            $list_array = $this->fetch_array($list);
+            return $list_array;
+        } else {
+            return false;
+        }
+
+    }
+
+    public function fetch_one_assoc($sql)
+    {
+
+        $list = $this->query($sql);
+
+        if ($list) {
             $list_array = $this->fetch_assoc($list);
             return $list_array;
-        }
-        else
+        } else {
             return false;
+        }
+
     }
 
-    function fetch_all_array($sql,$max=0)
+    public function fetch_all_array($sql, $max = 0)
     {
-        if(is_string($sql))    
+        if (is_string($sql)) {
             $query = $this->query($sql);
-        else
+        } else {
             $query = $sql;
+        }
 
         $all_array = array();
-        while($list_item = $this->fetch_array($query))
-        {
-            $current_index ++;
-            
-            if($current_index > $max && $max != 0)
-            {
+        while ($list_item = $this->fetch_array($query)) {
+            $current_index++;
+
+            if ($current_index > $max && $max != 0) {
                 break;
             }
-            
+
             $all_array[] = $list_item;
-            
+
         }
-        
-        return $all_array;    
+
+        return $all_array;
     }
 
-    function fetch_all_assoc($sql,$max=0)
+    public function fetch_all_assoc($sql, $max = 0)
     {
         $current_index = 0;
-        $all_array = array();
+        $all_array     = array();
 
-        if(is_string($sql)){
+        if (is_string($sql)) {
             $query = $this->mi->query($sql);
 
-            if($query===false)
-            {
-                return false ;
+            if ($query === false) {
+                return false;
             }
-        }    
-        else if(is_object($sql)){
+        } else if (is_object($sql)) {
             $query = $sql;
+        } else {
+            return false;
         }
-        else
-            return false ;
 
         /*
         if($query===false)
         {
-            return false;
+        return false;
         }
-        */
-                
-        while($list_item = $query->fetch_assoc())
-        {
+         */
 
-            $current_index ++;
-            
-            if($current_index > $max && $max != 0)
-            {
+        while ($list_item = $query->fetch_assoc()) {
+
+            $current_index++;
+
+            if ($current_index > $max && $max != 0) {
                 break;
             }
-            
+
             $all_array[] = $list_item;
-            
+
         }
-        
+
         return $all_array;
     }
 
-    function fetch_all_object($sql,$max=0)
+    public function fetch_all_object($sql, $max = 0)
     {
         $current_index = 0;
-        $result = $this->query($sql);
+        $result        = $this->query($sql);
 
         $all_array = array();
-        if ($result) 
-        {
+        if ($result) {
             /* fetch object array */
-            while ($obj = $result->fetch_object()) 
-            {
-                $current_index ++;
-                
-                if($current_index > $max && $max != 0)
-                {
+            while ($obj = $result->fetch_object()) {
+                $current_index++;
+
+                if ($current_index > $max && $max != 0) {
                     break;
                 }
-                
+
                 $all_array[] = $obj;
             }
 
             /* free result set */
             $result->close();
             return $all_array;
-        }
-        else
+        } else {
             return false;
+        }
+
     }
 
     /**
-    * 定义添加数据的方法
-    * @param string $table 表名
-    * @param string orarray $data [数据]
-    * @return int 最新添加的id
-    */
-    public function insert($table,$data)
+     * 定义添加数据的方法
+     * @param string $table 表名
+     * @param string orarray $data [数据]
+     * @return int 最新添加的id
+     */
+    public function insert($table, $data)
     {
-        //遍历数组，得到每一个字段和字段的值
-        $key_str='';
-        $v_str='';
-        foreach($data as $key=>$v)
-        {
-            if(empty($v))
-                return false;
-            
-            //$key的值是每一个字段s一个字段所对应的值
-            $key_str    .= $key.',';
-            $v_str      .= "'$v',";
+//遍历数组，得到每一个字段和字段的值
+        $key_str = '';
+        $v_str   = '';
+
+        foreach ($data as $key => $v) {
+            if (empty($v)) {
+                //值为0时会有BUG
+                if (!is_numeric($v)) {
+                    return false;
+
+                }
+
+            }
+
+//$key的值是每一个字段s一个字段所对应的值
+            $key_str .= '`'.$key . '`,';
+
+            if (stripos($v, "PASSWORD(") === 1) {
+
+                $v_str .= " $v,";
+            } else {
+                $v_str .= "'$v',";
+            }
         }
+        $key_str = trim($key_str, ',');
+        $v_str   = trim($v_str, ',');
 
-        $key_str    = trim($key_str,',');
-        $v_str      = trim($v_str,',');
+//判断数据是否为空
+        $sql = "insert into $table ($key_str) values ($v_str)";
+//file_put_contents("a.txt",$sql,FILE_APPEND);
+        // echo $sql;
+        // echo "\r\n";
+// die;        
 
-        //判断数据是否为空
-        $sql="INSERT into $table ($key_str) values ($v_str)";
         $this->query($sql);
-
-        //返回上一次增加操做产生ID值
+//返回上一次增加操做产生ID值
         return $this->insert_id();
     }
 
-    /**
-    * [修改操作description]
-    * @param [type] $table [表名]
-    * @param [type] $data [数据]
-    * @param [type] $where [条件]
-    * @return [type]
-    */
-    public function update($table,$data,$where)
+/**
+ * [修改操作description]
+ * @param [type] $table [表名]
+ * @param [type] $data [数据]
+ * @param [type] $where [条件]
+ * @return [type]
+ */
+    public function update($table, $data, $where)
     {
-        //遍历数组，得到每一个字段和字段的值
-        $str='';
 
-        foreach($data as $key=>$v)
-        {
-            $str.="$key='$v',";
+//遍历数组，得到每一个字段和字段的值
+        $str = '';
+        foreach ($data as $key => $v) {
+            $str .= "$key='$v',";
         }
-
-        $str=rtrim($str,',');
-
-        //修改SQL语句
-        $sql="UPDATE $table set $str where $where";
+        $str = rtrim($str, ',');
+//修改SQL语句
+        $sql = "update $table set $str where $where";
+// echo $sql;die;
         $this->query($sql);
-        
-        //返回受影响的行数
-        return $this->affected_rows();
+//返回受影响的行数
+        if($this->affected_rows()>=0){
+            return 1;
+        }else{
+
+        return 0;
+        }
+    }
+
+
+       /**
+     * 定义添加数据的方法
+     * @param string $table 表名
+     * @param string orarray $data [数据]
+     * @return int 最新添加的id
+     */
+    public function replaceInto($table, $data)
+    {
+//遍历数组，得到每一个字段和字段的值
+        $key_str = '';
+        $v_str   = '';
+
+        foreach ($data as $key => $v) {
+            if (empty($v)) {
+                //值为0时会有BUG
+                if (!is_numeric($v)) {
+                    return false;
+
+                }
+
+            }
+
+//$key的值是每一个字段s一个字段所对应的值
+            $key_str .= '`'.$key . '`,';
+
+            if (stripos($v, "PASSWORD(") === 1) {
+
+                $v_str .= " $v,";
+            } else {
+                $v_str .= "'$v',";
+            }
+        }
+        $key_str = trim($key_str, ',');
+        $v_str   = trim($v_str, ',');
+
+//判断数据是否为空
+        $sql = "replace into $table ($key_str) values ($v_str)";
+//file_put_contents("a.txt",$sql,FILE_APPEND);
+        // echo $sql;die;
+        $this->query($sql);
+//返回上一次增加操做产生ID值
+        return $this->insert_id();
     }
 
 }
